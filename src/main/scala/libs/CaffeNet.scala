f542d152ae86894aba75a12055700f9841930636
Philipp Moritz <pcmoritz@gmail.com>
2016-01-16 15:24:19 -0800

    implementing forward and backward for CaffeNet

--- src/main/scala/libs/CaffeNet.scala
+++ src/main/scala/libs/CaffeNet.scala
@@ -13,9 +13,11 @@ trait NetInterface {
 
 class JavaCPPCaffeNet(netParam: NetParameter, schema: StructType, preprocessor: Preprocessor) {
   private val inputSize = netParam.input_size
+  private val batchSize = netParam.input_shape(0).dim(0).toInt
   private val transformations = new Array[Any => NDArray](inputSize)
   private val inputIndices = new Array[Int](inputSize)
   private val columnNames = schema.map(entry => entry.name)
+  private val caffeNet = new FloatNet(netParam)
 
   for (i <- 0 to inputSize) {
     val name = netParam.input(i).getString
@@ -23,7 +25,17 @@ class JavaCPPCaffeNet(netParam: NetParameter, schema: StructType, preprocessor: 
     inputIndices(i) = columnNames.indexOf(name)
   }
 
-  def transformInto(iterator: Iterator[Row], data: FloatBlobVector) = {
+  // Preallocate a buffer for data input into the net
+  val inputs = new FloatBlobVector(inputSize)
+  for (i <- 0 to inputSize) {
+    val dims = new Array[Int](netParam.input_shape(i).dim_size)
+    for (j <- dims.indices) {
+      dims(j) = netParam.input_shape(i).dim(j).toInt
+    }
+    inputs.put(i, new FloatBlob(dims))
+  }
+
+  def transformInto(iterator: Iterator[Row], data: FloatBlobVector): Unit = {
     var batchIndex = 0
     while (iterator.hasNext) {
       val row = iterator.next
@@ -37,6 +49,26 @@ class JavaCPPCaffeNet(netParam: NetParameter, schema: StructType, preprocessor: 
         buffer.put(flatArray: _*)
       }
       batchIndex += 1
+      if (batchIndex == batchSize) {
+        return
+      }
     }
+  }
+
+  def forward(rowIt: Iterator[Row]): Array[Row] = {
+    val result = new Array[Array[Float]](batchSize)
+    transformInto(rowIt, inputs)
+    val tops = caffeNet.Forward(inputs)
+    for (i <- 0 to batchSize) {
+      val top = tops.get(i).cpu_data()
+      val array = Array[Float](top.position())
+      top.get(result(i))
+    }
+    return result.map(row => Row(row))
+  }
+
+  def forwardBackward(rowIt: Iterator[Row]) = {
+    transformInto(rowIt, inputs)
+    caffeNet.ForwardBackward(inputs)
   }
 }
